f1fb38aa256046f52857e1331a099c1b764b3414
Askar4272 <askar4272@gmail.com>
2024-09-30 20:45:00 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    int(first)
+    int(second)
+    if second == 0:
+        b = 'Ошибка'
+    else:
+        b = first / second
+    return b
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+def divide(first, second):
+    int(first)
+    int(second)
+    if second == 0:
+       a = inf
+    else:
+        a = first/second
+    return a
